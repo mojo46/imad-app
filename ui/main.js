17e842a2c8dc3ed7e4b1ab42db1243b374a99f62
ManojKumar-P <manojmj96@hotmail.com>
2017-08-25 11:15:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
-var button = document.getElementById('counter');
+var button = document.getElementById("counter");
 var counter=0;
 button.onclick = function(){
     //Make the request to counter
@@ -8,7 +8,7 @@ button.onclick = function(){
     //renser the variable in the current span
     
     counter= counter+1;
-    var span= document.getElementById('count');
+    var span= document.getElementById("count");
     span.innerHTML = counter.toString();
     
     
