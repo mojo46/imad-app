f92d9c236c563b1463d502011d3d2e2abc5901f7
ManojKumar-P <manojmj96@hotmail.com>
2017-08-27 19:31:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -28,7 +28,7 @@ button.onclick= function(){
           if(request.status === 200){
               var counter = responseText;
               span = document.getElementById('s1');
-              document.write(span.toString());
+              document.write("span.toString()");
               span.innerHTML = counter.toString();
           }
       }  
